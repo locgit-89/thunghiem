e244fecfb4c235fd0845437aa780afa4c9a5d223
locgit-89 <thantheloc@gmail.com>
2025-11-16 16:24:39 +0700

    A commit lan 1

---
+++ testThu/be/data/file1.cs
@@ -0,0 +1,17 @@
+﻿namespace be.data
+{
+    public class file1
+    {
+        public string ten;
+        //
+        public file1(string t)
+        {
+            ten = t;
+        }
+        //
+        public void ham1()
+        {
+            Console.WriteLine("ten: " + ten);   
+        }
+    }
+}
